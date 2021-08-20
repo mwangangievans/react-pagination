b259514229b810d23c61e8d82e1019113e959054
root <root@kali.kali>
2021-08-20 04:45:25 -0400

    pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState , useEffect} from 'react'
-
+import './App.css';
+import Post from './components/Post';
 
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
@@ -16,6 +17,21 @@ const App = () => {
     .catch((error)=>setError(error.message));
   }, []);
   if (error)  return <h1>{error}</h1>;
-  return <div></div>;
+  return (
+    <div>
+      {posts.length > 0 ? (
+        <>
+        <Pagination data ={posts}
+        RenderComponents={Post}
+        title="posts"
+        pageLimit={5}
+        dataLimit={10}/>
+        </>
+      ) : (
+        <h1>No Post to display </h1>
+      )
+    }
+    </div>
+  );
 }
 export default App;
