1c9ceed7c892512bf87efc9fc46a563d57d5e3bf
root <root@kali.kali>
2021-08-20 02:24:02 -0400

    second  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState , useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+const App = () => {
+  const [posts , setPosts] = useState([]);
+  const [error , setError] = useState([]);
+  useEffect(()=>{
+    fetch(url)
+    .then((response)=>{
+      if(response.ok) return 
+      response.json();
+      throw new Error('something went wrong while requesting posts');
+    }).then((posts)=> setPosts(posts))
+    .catch((error)=>setError(error.message));
+  }, []);
+  if (error)  return <h1>{error}</h1>;
+  return <div></div>;
 }
-
 export default App;
